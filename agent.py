0d2f8e579e624b39a48e1217410e77ccbbd7d000
= <=>
2021-10-08 10:21:48 +0200

    Added my early work

--- agent.py
+++ agent.py
@@ -17,15 +17,39 @@ class Agent:
         self.is_binded_with = None
 
     def perception(self, environment):
-        row, col = environment.agents[self.key]
-        self.memory = environment.object_grid[row][col] + self.memory[0:-1]
+        pass
 
     def action(self):
         if not self.is_binded_with:
+
 
     def move(self, environment):
         position = environment.agents[self.key]
         environment.move(position)
         row, col = position
         self.memory = environment.object_grid[row][col] + self.memory[0:-1]
+
+        directions = [
+                (1,0), (1,1), (0,1), (-1,1),
+                (-1, 0), (-1, -1), (0, -1), (1, -1)
+            ]
+        random.shuffle(directions)
+
+        move = None
+
+
+        try:
+            # This loop will look for a valid move
+            while not move:
+                move = directions.pop()  # We choose randomly a direction
+
+                if self.is_valid_move(position, move):
+                    break  # A valid direction has been found
+                else:
+                    move = None  # The move wasn't valid
+
+        # No direction is valid so the agent doesn't move
+        except IndexError:
+            move = (0, 0)
+
 
---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Main module coding the loop
+
+@authors: Nathan Etourneau, Paul Flagel
+"""
+from agent import Agent
+from environment import Environment
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
